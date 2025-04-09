e428e374e64954043f2aca265cfe9180870b5812
tuqajr <tuqaislam2001@gmail.com>
2025-04-09 16:52:22 +0300

    api

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category = Category::create([
+            'name' => $request->name,
+        ]);
+
+        return response()->json($category, 201); 
+    }
+
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    public function show($id)
+    {
+        $category = Category::findOrFail($id);
+        return response()->json($category);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category->update([
+            'name' => $request->name,
+        ]);
+
+        return response()->json($category);
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response()->json(null, 204); 
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+// app/Models/Category.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    // Specify the fillable fields for mass assignment
+    protected $fillable = ['name'];
+}
+
